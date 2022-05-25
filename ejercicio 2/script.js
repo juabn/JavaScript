9f55e6e057956c6f4688eafb977863880357b067
juabn <gaboteamodemasiado@gmail.com>
2022-05-25 12:07:35 -0300

    puse letras en negrita

--- ejercicio 2/script.js
+++ ejercicio 2/script.js
@@ -1,8 +1,8 @@
 const href = window.location.href;
-document.write(`Href: ${href}<br><br><br>`);
+document.write(`Href: <b>${href}</b><br><br><br>`);
 const hostname = window.location.hostname;
-document.write(`Hostname: ${hostname}<br><br><br>`);
+document.write(`Hostname: <b>${hostname}</b><br><br><br>`);
 const pathname = window.location.pathname;
-document.write(`Pathname: ${pathname}<br><br><br>`);
+document.write(`Pathname: <b>${pathname}</b><br><br><br>`);
 const protocol = window.location.protocol;
-document.write(`Protocolo: ${protocol}<br><br><br>`);
+document.write(`Protocolo: <b>${protocol}</b><br><br><br>`);
