e7044b618db1e83684fa9814a49fa8d7e97649d3
juabn <gaboteamodemasiado@gmail.com>
2022-05-25 12:05:59 -0300

    pagina que te indica informacion sobre ella

---
+++ ejercicio 2/script.js
@@ -0,0 +1,8 @@
+const href = window.location.href;
+document.write(`Href: ${href}<br><br><br>`);
+const hostname = window.location.hostname;
+document.write(`Hostname: ${hostname}<br><br><br>`);
+const pathname = window.location.pathname;
+document.write(`Pathname: ${pathname}<br><br><br>`);
+const protocol = window.location.protocol;
+document.write(`Protocolo: ${protocol}<br><br><br>`);
