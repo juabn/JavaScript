41696af1872306b85327029b66b6ede8ce45d117
juabn <gaboteamodemasiado@gmail.com>
2022-05-26 17:22:20 -0300

    solucion 2

--- ejercicio 3/solucion 2/script.js
+++ ejercicio 3/solucion 2/script.js
@@ -2,11 +2,11 @@ const nombreAlumno = document.querySelector('.div_form_name');
 const mail = document.querySelector('.div_form_mail');
 const materiaAdeudada = document.querySelector('.div_form_materia');
 const boton = document.querySelector('.div_form_boton');
-const alumnosInscritos = ['maria','juan bautista','jose','pedro','rodrigo','josefina','paula','estelvina','juan ignacio'];
+const alumnosInscritos = ['maria','juan bautista','jose','pedro','rodrigo','josefina','paula','estelvina','juan ignacio', 'josefa','estelvina','carina'];
 const materias = ['psicologia','ciencia','matematicas','programacion','fisica'];
 const contenedorInfo = document.querySelector('.informacion');
 const hdnInput = document.querySelector('.div_form_hdninput');
-let baseDatos = [];
+
 
 class alumno{
     constructor(nombre,mail,materia){
@@ -48,8 +48,8 @@ function checkearDatos(){
             if(materias.includes(materiaA)){
 
                 alumnoN = new alumno(nombreA,mailA,materiaA);
-                baseDatos.push(alumnoN);
-                datosHdnInput(baseDatos);
+                datosHdnInput(alumnoN);
+                alert('Datos enviados exitosamente!');
             }
 
             else if(materias.includes(materiaA) === false){
