a962664437be7bd6913912b52c06585025e74210
juabn <gaboteamodemasiado@gmail.com>
2022-06-14 23:39:00 -0300

    toma videos y empece barra

--- galeria dinamica/script.js
+++ galeria dinamica/script.js
@@ -12,26 +12,47 @@ inputFile.addEventListener('change',()=>{
 const insertarImg = (ar)=>{
     const fragment = document.createDocumentFragment();
     for(let i = 0; i < ar.length; i++){
-        // console.log(ar[i]);
+        console.log(ar[i].type);
         const reader = new FileReader();
-        reader.readAsDataURL(ar[i]);
-        reader.addEventListener('load',(e)=>{
-            let newImg = document.createElement('IMG');
-            newImg.setAttribute('src',e.currentTarget.result);
-            newImg.classList = 'imagenes_item';
-            fragment.appendChild(newImg);
-            container.appendChild(fragment);
+        reader.readAsArrayBuffer(ar[i]);
+        reader.addEventListener('progress',(e)=>{
+            
+            let carga = (e.loaded / ar[i].size) * 100;
+            console.log(carga);
         })
+        reader.addEventListener('load',(e)=>{
+            if(ar[i].type.includes('video') === true){
+                let video = new Blob([new Uint8Array(e.currentTarget.result)],{type: ar[i].type});
+                let url = URL.createObjectURL(video);
+                let newVideo = document.createElement('VIDEO');
+                newVideo.classList = 'imagenes_item';
+                newVideo.setAttribute('src',url);
+                newVideo.autoplay = false;
+                fragment.appendChild(newVideo);
+                container.appendChild(fragment);
+            }
+
+            else if(ar[i].type.includes('image')){
+                let url = URL.createObjectURL(ar[i]);
+                let newImg = document.createElement('IMG');
+                newImg.setAttribute('src',url);
+                newImg.classList = 'imagenes_item';
+                fragment.appendChild(newImg);
+                container.appendChild(fragment);
+            }
+        })
+        
     }
 }
 
 dropimg.addEventListener('dragover',(e)=>{
     e.preventDefault();
-    console.log('over');
+   
 });
 dropimg.addEventListener('drop',(e)=>{
     e.preventDefault();
-    console.log(e);
+    
     insertarImg(e.dataTransfer.files);
+
 })
 
